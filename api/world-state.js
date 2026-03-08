bffb8170a453a89d33a1f190b4a85875e7f916e2
iMortaluz <yunustarar@gmail.com>
2026-03-08 05:22:44 +0300

    Update world-state.js

--- api/world-state.js
+++ api/world-state.js
@@ -1,9 +1,16 @@
-import worldState from '../world-state.json' assert { type: 'json' };
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const dataPath = path.join(__dirname, '..', 'world-state.json');
 
 export default function handler(req, res) {
   if (req.method === 'GET') {
+    const json = fs.readFileSync(dataPath, 'utf-8');
     res.setHeader('Cache-Control', 'no-cache');
-    return res.status(200).json(worldState);
+    return res.status(200).json(JSON.parse(json));
   }
   res.status(405).end();
 }
